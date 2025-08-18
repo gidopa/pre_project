08413cd6e8934019ba9793f32aa66fe43d883284
Gido <120196095+gidopa@users.noreply.github.com>
2025-08-18 16:57:15 +0900

    style : 줄바꿈

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-insert into users (user_id, username, password, email, user_role, created_at, is_deleted) values (RANDOM_UUID(),'admin','1234', '111@naver.com', 'ADMIN', CURRENT_TIMESTAMP, false)
+insert into users (user_id, username, password, email, user_role, created_at, is_deleted) values
+(RANDOM_UUID(),'admin','1234', '111@naver.com', 'ADMIN', CURRENT_TIMESTAMP, false);
 
 
 
